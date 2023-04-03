f550fbea42c45438523c067aa8a2aa7005da14ab
regurusu7257 <rezero.2.5.3@gmail.com>
2023-04-03 23:25:25 +0900

    Initial commit

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private float speed = -12;
+
+    private float deadLine = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+
+        if (other.gameObject.tag == "groundtag" || other.gameObject.tag == "cubetag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
